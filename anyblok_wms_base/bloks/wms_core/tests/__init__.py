3e8b0a66f083020ec5021b36d23815e147eee90a
Georges Racinet <georges@racinet.fr>
2018-01-26 14:09:56 +0100

    Bootstrapped tests and Travis configuration

---
+++ anyblok_wms_base/bloks/wms_core/tests/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# This file is a part of the AnyBlok / WMS Base project
+#
+#    Copyright (C) 2018 Georges Racinet <gracinet@anybox.fr>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file,You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+from anyblok.tests.testcase import BlokTestCase
+
+
+class TestCore(BlokTestCase):
+
+    def test_insert_items_type(self):
+        items_type = self.registry.Wms.Items.Type.insert(label="My items type")
+        self.assertEqual(items_type.label, "My items type")
