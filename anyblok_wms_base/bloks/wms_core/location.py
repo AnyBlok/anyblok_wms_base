6b1ab3da785e71405f5d832ca842a907379f3149
Georges Racinet <georges@racinet.fr>
2018-02-06 16:59:46 +0100

    First, non recursive stock level method
    For now, it takes states into account, but that's about it

--- anyblok_wms_base/bloks/wms_core/location.py
+++ anyblok_wms_base/bloks/wms_core/location.py
@@ -7,6 +7,7 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
 
+from sqlalchemy import func
 from anyblok import Declarations
 from anyblok.column import String
 from anyblok.column import Integer
@@ -28,3 +29,26 @@ class Location:
     label = String(label="Label")
     parent = Many2One(label="Parent location",
                       model='Model.Wms.Location')
+
+    def quantity(self, goods_type, goods_state='present'):
+        """Return the full quantity present in location for the given type.
+
+        This doesn't take potential of Unpacks into account, so it's not
+        readily suited for the question "do I have this in stock ?"
+
+        TODO: make recursive (not fully decided about the forest structure
+        of locations)
+        TODO: provide filtering according to Goods properties (should become
+        special PostgreSQL JSON clauses)
+        TODO: add date filtering to peek into past, or do this on some
+        other method
+        """
+        Goods = self.registry.Wms.Goods
+        query = Goods.query(func.sum(Goods.quantity)).filter(
+            Goods.type == goods_type, Goods.location == self)
+        if goods_state == 'future':
+            query = query.filter(Goods.state.in_(('future', 'present')))
+        else:
+            # this is where we'd be really happy with an Enum
+            query = query.filter(Goods.state == goods_state)
+        return query.one()[0]
---
+++ anyblok_wms_base/bloks/wms_core/tests/test_location.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# This file is a part of the AnyBlok / WMS Base project
+#
+#    Copyright (C) 2018 Georges Racinet <gracinet@anybox.fr>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file,You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+from anyblok.tests.testcase import BlokTestCase
+from decimal import Decimal as D
+
+
+class TestGoods(BlokTestCase):
+
+    blok_entry_points = ('bloks', 'test_bloks')
+
+    def setUp(self):
+        Wms = self.registry.Wms
+
+        self.Goods = Wms.Goods
+        self.goods_type = self.Goods.Type.insert(label="My goods")
+        self.stock = Wms.Location.insert(label="Stock")
+        self.arrival = Wms.Operation.Arrival.insert(
+            goods_type=self.goods_type,
+            location=self.stock,
+            state='done',
+            quantity=5)
+
+    def insert_goods(self, qty, state):
+        self.Goods.insert(type=self.goods_type, quantity=qty,
+                          reason=self.arrival, location=self.stock,
+                          state=state)
+
+    def assertQuantity(self, quantity, **kwargs):
+        self.assertEqual(
+            self.stock.quantity(self.goods_type, **kwargs),
+            quantity)
+
+    def test_quantity(self):
+        self.insert_goods(1, 'present')
+        self.insert_goods(0.5, 'present')
+        self.insert_goods(2, 'future')
+        self.insert_goods(1, 'past')
+
+        self.assertQuantity(D('1.5'))
+        self.assertQuantity(D('1.5'), goods_state='present')
+        self.assertQuantity(D('3.5'), goods_state='future')
+        self.assertQuantity(1, goods_state='past')
