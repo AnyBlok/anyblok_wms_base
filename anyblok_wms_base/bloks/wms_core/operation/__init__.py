e07e800609d95ed5aa4a62492238ea05805ae133
Georges Racinet <georges@racinet.fr>
2018-02-27 15:47:04 +0100

    Renamed on_goods module as 'single_input'
    Made no sense anymore: it only has the SingleInput mixin, now

--- anyblok_wms_base/bloks/wms_core/operation/__init__.py
+++ anyblok_wms_base/bloks/wms_core/operation/__init__.py
@@ -9,7 +9,7 @@
 # obtain one at http://mozilla.org/MPL/2.0/.
 
 from . import base
-from . import on_goods
+from . import single_input
 from . import splitter
 from . import arrival
 from . import departure
