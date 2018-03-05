a325d76cc6db3705ab263a32200e5dd55e013736
Georges Racinet <georges@racinet.fr>
2018-03-05 14:22:03 +0100

    wms-reservation: locking and reserving
    This is the second part of the reservation logic:
    looking up Goods satisfying the Requests and actually
    reserving them.
    It comes with everything that should be needed reserver
    processes, notably locking.
    
    Except in the largest applications, reservations should be
    done in-order, but there is some capability to cut the stack in
    independent parts, by using the query_filter parameter in reserve_all().
    Of course it's then the caller's responsibility that (functionally) the
    ordering stays acceptable.

--- anyblok_wms_base/bloks/wms_reservation/request.py
+++ anyblok_wms_base/bloks/wms_reservation/request.py
@@ -6,9 +6,12 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
+import time
 from contextlib import contextmanager
 
+import sqlalchemy
 from sqlalchemy import CheckConstraint
+from sqlalchemy import func
 
 from anyblok import Declarations
 from anyblok.column import Integer
@@ -131,6 +134,111 @@ class Request:
         """
         return self.id in self.txn_owned_reservations
 
+    def reserve(self):
+        """Try and perform reservation for all RequestItems.
+
+        Should not fail if reservations are already done.
+
+        :return: ``True`` if all reservations are taken
+        :rtype: bool
+        """
+        Item = self.registry.Wms.Reservation.RequestItem
+        # could use map() and all(), but it's not recommended style
+        # if there are strong side effects.
+        all_reserved = True
+        for item in Item.query().filter(Item.request == self).all():
+            all_reserved = all_reserved and item.reserve()
+        self.reserved = all_reserved
+        return all_reserved
+
+    @classmethod
+    def lock_unreserved(cls, batch_size, query_filter=None, offset=0):
+        """Take exclusivity over not yet reserved Requests
+
+        This is used by :ref:`reservers <arch_reserver>`.
+
+        :param int batch: maximum of reservations to lock at once.
+
+        Since reservations have to be taken in order, this produces a hard
+        error in case there's a conflicting database lock, instead of skipping
+        them like :meth:`claim_reservations` does.
+
+        This conflicts in particular locks taken with
+        :meth`claim_reservations`, but in principle,
+        only :ref:`reservers arch_reserver should take locks
+        over reservation Requests that are not reserved yet, and these should
+        not run in concurrency (or in a very controlled way, using
+        ``query_filter``).
+        """
+        query = cls.query().filter(cls.reserved.is_(False))
+        if query_filter is not None:
+            query = query_filter(query)
+        query = query.with_for_update(nowait=True).order_by(cls.id)
+        try:
+            return query.limit(batch_size).offset(offset).all()
+        except sqlalchemy.exc.OperationalError as op_err:
+            cls.registry.rollback()
+            raise cls.ReservationsLocked(op_err)
+
+    class ReservationsLocked(RuntimeError):
+        """Used to rewrap concurrency errors while taking locks."""
+        def __init__(self, db_exc):
+            self.db_exc = db_exc
+
+    @classmethod
+    def reserve_all(cls, batch_size=10, nb_attempts=5, retry_delay=1,
+                    query_filter=None):
+        """Try and perform all reservations for pending Requests.
+
+        This walks all pending (:attr:`reserved` equal to ``False``)
+        Requests that haven't been reserved from the oldest and locks
+        them by batches of ``batch_size``.
+
+        Reservation is attempted for each request, in order, meaning that
+        each request will grab as much Goods as it can before the next one
+        gets processed.
+
+        :param int batch_size:
+           number of pending Requests to grab at each iteration
+        :param nb_attempts:
+           number of attempts (in the face of conflicts) for each batch
+        :param retry_delay:
+           time to wait before retrying to grab a batch (hoping other
+           transactions holding locks would have released them)
+        :param query_filter:
+           optional function to add filtering to the query used to grab the
+           reservations. The caller can use this to implement controlled
+           concurrency in the reservation process: several processes can
+           focus on different Requests, as long as they don't compete for
+           Goods to reserve.
+
+        The transaction is committed for each batch.
+        """
+        skip = 0
+        while True:
+            # TODO log.info
+            count = 1
+            while True:
+                try:
+                    requests = cls.lock_unreserved(batch_size,
+                                                   offset=skip,
+                                                   query_filter=query_filter)
+                except cls.ReservationsLocked:
+                    # TODO log.warning
+                    if count == nb_attempts:
+                        raise
+                    time.sleep(retry_delay)
+                    count += 1
+                else:
+                    break
+            if not requests:
+                break
+
+            for request in requests:
+                if not request.reserve():
+                    skip += 1
+            cls.registry.commit()
+
 
 @register(Wms.Reservation)
 class RequestItem:
@@ -155,3 +263,71 @@ class RequestItem:
         return super(RequestItem, cls).define_table_args() + (
             CheckConstraint('quantity > 0', name='positive_qty'),
         )
+
+    def lookup(self, quantity):
+        """Try and find Goods matchin the specified conditions.
+
+        :return: the matching Goods that were found and the quantity each
+                 accounts for. The Goods may not be of the requested type.
+                 What matters is how much of the requested quantity
+                 each one represents.
+
+        :rtype: list(int, :class:`Goods
+                   <anyblok_wms_base/bloks/wms_core/goods.Goods`>)
+
+        This method is where most business logic should lie.
+
+        This default
+        implementation does only equal matching on Goods Type and each
+        property, and therefore is not able to return other Goods Type
+        accounting for more than one of the wished.
+        Downstream libraries and applications are welcome to override it.
+        """
+        Wms = self.registry.Wms
+        Goods = Wms.Goods
+        Reservation = Wms.Reservation
+        Avatar = Goods.Avatar
+        Props = Goods.Properties
+        # TODO PERF this returns from the DB one Goods line per
+        # Avatar, but SQLA reassembles them as exactly one (seen while
+        # tracing the test_reserve_avatars_once() under pdb)
+        # SELECT DISTINCT ON would be better
+        # TODO provide ordering by Avatar state and/or dt_from
+        query = Goods.query().join(Avatar.goods).outerjoin(
+            Reservation, Reservation.goods_id == Goods.id).filter(
+                Reservation.goods_id.is_(None),
+                Goods.type == self.goods_type,
+                Avatar.state.in_(('present', 'future')))
+        props = self.properties.copy()
+        if props:
+            query = query.join(Goods.properties)
+            pfields = Props.fields_description()
+            for p in set(props).intersection(pfields):
+                query = query.filter(getattr(Props, p) == props.pop(p))
+            if props:
+                query = query.filter(Props.flexible.contains(props))
+        return [(1, g) for g in query.limit(quantity).all()]
+
+    def reserve(self):
+        """Perform the wished reservations.
+
+        :return bool: if the RequestItem is completely reserved.
+        # TODO shall we store it directly in DB ?
+        """
+        Reservation = self.registry.Wms.Reservation
+        already = Reservation.query(func.sum(Reservation.quantity)).filter(
+            Reservation.request_item_id == self.id).one()[0]
+        if already is None:
+            already = 0
+        if already >= self.quantity:
+            # its legit to be greater, think of reserving 2 packs of 10
+            # to use 17. Maybe later, we'll unpack just one of them and update
+            # the reservation to add just 7 of the Unpack outcomes.
+            return True
+        added = 0
+        for quantity, goods in self.lookup(self.quantity - already):
+            # TODO use a o2m ?
+            Reservation.insert(goods=goods, quantity=quantity,
+                               request_item=self)
+            added += quantity
+        return already + added >= self.quantity
--- anyblok_wms_base/bloks/wms_reservation/reservation.py
+++ anyblok_wms_base/bloks/wms_reservation/reservation.py
@@ -6,7 +6,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
+from sqlalchemy import CheckConstraint
 from anyblok import Declarations
+from anyblok.column import Integer
 from anyblok.relationship import Many2One
 
 register = Declarations.register
@@ -17,8 +19,28 @@ Wms = Declarations.Model.Wms
 class Reservation:
 
     goods = Many2One(model=Wms.Goods, primary_key=True, index=True)
+    quantity = Integer()
+    """The quantity that this Reservation provides.
+
+    If the Goods in the application have ``quantity`` field
+    (see :ref:`improvement_no_quantities`), this is not necessarily its value
+    within :attr:`goods`. Instead, it is the quantity within the
+    :attr:`request_item` that the current Reservation provides.
+
+    Use-case some Goods being sold either as packs of 10 or by
+    the unit. If one wants to reserve 13 of them,
+    it should be expressable as one pack of 10 and 3 units.
+    Then maybe (depending on the needs), would it be actually
+    smarter of the application to not issue an Unpack.
+    """
     request_item = Many2One(model=Wms.Reservation.RequestItem,
                             index=True)
+
+    @classmethod
+    def define_table_args(cls):
+        return super(Reservation, cls).define_table_args() + (
+            CheckConstraint('quantity > 0', name='positive_qty'),
+        )
 
     def is_transaction_owner(self):
         """Check that the current transaction is the owner of the reservation.
