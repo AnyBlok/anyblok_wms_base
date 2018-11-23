f67a5bac279a60b7086c4e576f99215e1a5528b5
Georges Racinet <georges@racinet.fr>
2018-11-23 23:26:40 +0100

    Bringing self-coverage of operation subpackages to 100%
    Well, unsurprisingly, that was easy

--- anyblok_wms_base/quantity/operation/tests/__init__.py
+++ anyblok_wms_base/quantity/operation/tests/__init__.py
@@ -6,3 +6,17 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
+from anyblok.tests.testcase import BlokTestCase
+from .. import import_declarations
+
+
+class TestOperationPackage(BlokTestCase):
+
+    def test_reload(self):
+        import sys
+        module_type = sys.__class__  # is there a simpler way ?
+
+        def fake_reload(module):
+            self.assertIsInstance(module, module_type)
+
+        import_declarations(reload=fake_reload)
